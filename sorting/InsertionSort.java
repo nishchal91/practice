ff42e523c709c4ed87af615412ff872086b995f2
nishchal91 <nishchal91@gmail.com>
2018-01-03 16:44:37 -0500

    Insertion sort

---
+++ sorting/InsertionSort.java
@@ -0,0 +1,25 @@
+package com.practice.sorting;
+
+public class InsertionSort {
+	
+	public void insertionSort(int[] array){
+		for(int i=1;i<array.length;i++){
+			int num=array[i];
+			int j=i-1;
+			while(j>=0 && array[j]> num){
+				array[j+1]=array[j];
+				j=j-1;
+			}
+			array[j+1]=num;
+		}
+	}
+	public static void main(String[] args) {
+		InsertionSort sort=new InsertionSort();
+		int[] arr={2,9,3,1,4,6,5};
+		sort.insertionSort(arr);
+		for(int i=0;i<arr.length;i++){
+			System.out.println(arr[i]);
+		}
+	}
+
+}
